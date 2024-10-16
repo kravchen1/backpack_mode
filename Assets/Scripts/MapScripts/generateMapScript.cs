fd2c029a36ca25941bd41da2f02fa1964ba87dc8
Aleksej Lapin <alapin@conteq.com>
2024-10-16 18:02:18 +0300

    новый вид генерации карты

--- Assets/Scripts/MapScripts/generateMapScript.cs
+++ Assets/Scripts/MapScripts/generateMapScript.cs
@@ -150,143 +150,201 @@ public class generateMapScript : Map
             this.canBuild = canBuild;
         }
     }
-    public void MapGenerator(InterestPointStructure carePoint)
-    {
-        if (carePoint.canBuild)
-        {
-            var canBuild = Random.Range(0, 2);
-            if (canBuild == 0 && roadToBoss)
-            {
-                carePoint.canBuild = false;
-                return;
-            }
-            Dictionary<int, VectorStructure> movingVectors = new Dictionary<int, VectorStructure>();
-            movingVectors.Add(0, new VectorStructure(new Vector2(0, stepSize)));
-            movingVectors.Add(1, new VectorStructure(new Vector2(0, -stepSize)));
-            movingVectors.Add(2, new VectorStructure(new Vector2(-stepSize, 0)));
-            movingVectors.Add(3, new VectorStructure(new Vector2(stepSize, 0)));
-            foreach (var vector in movingVectors)
-            {
-                if (tiles.Any(e => e.tilePosition == endPointTile.GetComponent<RectTransform>().anchoredPosition + vector.Value.movingVector))
-                {
-                    roadToBoss = true;
-                }
-            }
 
-            var bossPos = endPointTile.GetComponent<RectTransform>().anchoredPosition;
-            var newCarePoint = carePoint.position;
-            if ((carePoint.position + movingVectors[0].movingVector).y >= height || tiles.Any(e => e.tilePosition == carePoint.position + movingVectors[0].movingVector))
-                movingVectors[0].canMove = false;
-            else
-                movingVectors[0].canMove = true;
-            if ((carePoint.position + movingVectors[1].movingVector).y < 0 || tiles.Any(e => e.tilePosition == carePoint.position + movingVectors[1].movingVector))
-                movingVectors[1].canMove = false;
-            else
-                movingVectors[1].canMove = true;
-            if ((carePoint.position + movingVectors[2].movingVector).x < 0 || tiles.Any(e => e.tilePosition == carePoint.position + movingVectors[2].movingVector))
-                movingVectors[2].canMove = false;
-            else
-                movingVectors[2].canMove = true;
-            if ((carePoint.position + movingVectors[3].movingVector).x >= width || tiles.Any(e => e.tilePosition == carePoint.position + movingVectors[3].movingVector))
-                movingVectors[3].canMove = false;
-            else
-                movingVectors[3].canMove = true;
-            Dictionary<int, Vector2> existVectors = new Dictionary<int, Vector2>();
-            int vectorIndex = 0;
-            foreach (var vector in movingVectors.Where(e => e.Value.canMove == true))
+
+    public void GenerateSquare(float width, float heigth, bool isFirstSquare = true)
+    {
+        var exitPos = Random.Range(1, (int)(width / stepSize)-1);
+        for (int i = 0; i <= width; i += (int)stepSize)
+        {
+            var position = new Vector2(i, 0);
+            if (isFirstSquare)
             {
-                existVectors.Add(vectorIndex, vector.Value.movingVector);
-                vectorIndex++;
-            }
-            var randomVector = Random.Range(0, existVectors.Count());
-            if (existVectors.Count() == 0)
-            {
-                return;
+                generateTile(greenStandart, position);
+                tiles.Add(new Tile(greenStandart.name, position));
+                generateTile(treeStandart1, position);
+                tiles.Add(new Tile(treeStandart1.name, position));
+
             }
             else
             {
-                var randomRoadCount = Random.Range(1, 3);
-                for (int i = 0; i < randomRoadCount; i++)
+                if (!tiles.Any(e => e.tilePosition == position))
                 {
-                    newCarePoint += existVectors[randomVector];
-                    if (!tiles.Any(e => e.tilePosition == newCarePoint) && newCarePoint.x >= 0 && newCarePoint.y >= 0 && newCarePoint.x < width && newCarePoint.y < height - ((height % stepSize)))
+                    if (i != exitPos * stepSize)
                     {
-                        tiles.Add(new Tile(road.name,newCarePoint));
-                        generateTile(road, newCarePoint);
-                    }
-                    else
-                        break;
-                }
-                int randomPlace = Random.Range(1, 10);
-                int randomBattlePoint = 0;
-                newCarePoint += existVectors[randomVector];
-                if (!tiles.Any(e => e.tilePosition == newCarePoint) && newCarePoint.x >= 0 && newCarePoint.y >= 0 && newCarePoint.x < width && newCarePoint.y < height - ((height % stepSize)))
-                {
-                    //pointInterestPoisitions.Add(newCarePoint);
-                    pointInterestStructure.Add(new InterestPointStructure(newCarePoint));
-                    switch (randomPlace)
-                    {
-                        case 1:
-                            generateTile(fountainPrefab, newCarePoint);
-                            tiles.Add(new Tile(fountainPrefab.name, newCarePoint));
-                            break;
-                        case 2:
-                            generateTile(ChestOfFortune, newCarePoint);
-                            tiles.Add(new Tile(ChestOfFortune.name, newCarePoint));
-                            break;
-                        //case 3:
-                        //    generateTile(Forge, newCarePoint);
-                        //    tiles.Add(new Tile(Forge.name, newCarePoint));
-                        //    break;
-                        default:
-                            randomBattlePoint = Random.Range(1, 6);
-                            switch(randomBattlePoint)
-                            {
-                                case 1:
-                                    generateTile(battlePoint1, newCarePoint);
-                                    tiles.Add(new Tile(battlePoint1.name, newCarePoint));
-                                    break;
-                                case 2:
-                                    generateTile(battlePoint2, newCarePoint);
-                                    tiles.Add(new Tile(battlePoint2.name, newCarePoint));
-                                    break;
-                                case 3:
-                                    generateTile(battlePoint3, newCarePoint);
-                                    tiles.Add(new Tile(battlePoint3.name, newCarePoint));
-                                    break;
-                                case 4:
-                                    generateTile(battlePoint4, newCarePoint);
-                                    tiles.Add(new Tile(battlePoint4.name, newCarePoint));
-                                    break;
-                                case 5:
-                                    generateTile(battlePoint5, newCarePoint);
-                                    tiles.Add(new Tile(battlePoint5.name, newCarePoint));
-                                    break;
-                            }
-                            break;
-                            
+                        generateTile(greenStandart, position);
+                        tiles.Add(new Tile(greenStandart.name, position));
+                        generateTile(treeStandart1, position);
+                        tiles.Add(new Tile(treeStandart1.name, position));
                     }
                 }
             }
         }
-        else
-            return;
+        for (int i = 0; i <= width; i += (int)stepSize)
+        {
+            var position = new Vector2(i, heigth);
+            if (isFirstSquare)
+            {
+                generateTile(greenStandart, position);
+                tiles.Add(new Tile(greenStandart.name, position));
+                generateTile(treeStandart1, position);
+                tiles.Add(new Tile(treeStandart1.name, position));
+            }
+            else
+            {
+                if (!tiles.Any(e => e.tilePosition == position))
+                {
+                    if (i != exitPos * stepSize)
+                    {
+                        generateTile(greenStandart, position);
+                        tiles.Add(new Tile(greenStandart.name, position));
+                        generateTile(treeStandart1, position);
+                        tiles.Add(new Tile(treeStandart1.name, position)); ;
+                    }
+                }
+            }
+        }
+        exitPos = Random.Range(1, (int)(heigth / stepSize)-1);
+        for (int i = 0; i <= heigth; i += (int)stepSize)
+        {
+            var position = new Vector2(0, i);
+            if (isFirstSquare)
+            {
+                generateTile(greenStandart, position);
+                tiles.Add(new Tile(greenStandart.name, position));
+                generateTile(treeStandart1, position);
+                tiles.Add(new Tile(treeStandart1.name, position));
+            }
+            else
+            {
+                if (!tiles.Any(e => e.tilePosition == position))
+                {
+                    if (i != exitPos * stepSize)
+                    {
+                        generateTile(greenStandart, position);
+                        tiles.Add(new Tile(greenStandart.name, position));
+                        generateTile(treeStandart1, position);
+                        tiles.Add(new Tile(treeStandart1.name, position));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i <= heigth; i += (int)stepSize)
+        {
+            var position = new Vector2(width, i);
+            if (isFirstSquare)
+            {
+                generateTile(greenStandart, position);
+                tiles.Add(new Tile(greenStandart.name, position));
+                generateTile(treeStandart1, position);
+                tiles.Add(new Tile(treeStandart1.name, position));
+            }
+            else
+            {
+                if (!tiles.Any(e => e.tilePosition == position))
+                {
+                    if (i != exitPos * stepSize)
+                    {
+                        generateTile(greenStandart, position);
+                        tiles.Add(new Tile(greenStandart.name, position));
+                        generateTile(treeStandart1, position);
+                        tiles.Add(new Tile(treeStandart1.name, position));
+                    }
+                }
+            }
+        }
+    }
+
+    public void MapGenerator()
+    {
+        var randomSqaureCount = Random.Range(3, 7);
+        var newWidth = width - (width % stepSize);
+        var newHeigth = height - (height % stepSize);
+        GenerateSquare(newWidth, newHeigth);
+        for (int i = 0; i < randomSqaureCount; i++)
+        {
+            var randomSide = Random.Range(0, 2);
+            switch(randomSide)
+            {
+                case 0:
+                    newWidth = Random.Range(4, (int)(newWidth / stepSize) -1) * stepSize;
+                    break;
+                case 1:
+                    newHeigth = Random.Range(4, (int)(newHeigth / stepSize) - 1) * stepSize;
+                    break;
+            }
+            GenerateSquare(newWidth, newHeigth, false);
+            //newHeigth = newHeigth / 2;
+        }
+        //for (int i = 0; i < width; i += (int)stepSize)
+        //{
+        //    if (!tiles.Any(e => e.tilePosition == newCarePoint) && newCarePoint.x >= 0 && newCarePoint.y >= 0 && newCarePoint.x < width && newCarePoint.y < height - ((height % stepSize)))
+        //    {
+        //        tiles.Add(new Tile(road.name, newCarePoint));
+        //        generateTile(road, newCarePoint);
+        //    }
+        //    else
+        //        break;
+        //}
+        //int randomPlace = Random.Range(1, 10);
+        //int randomBattlePoint = 0;
+        //newCarePoint += existVectors[randomVector];
+        //if (!tiles.Any(e => e.tilePosition == newCarePoint) && newCarePoint.x >= 0 && newCarePoint.y >= 0 && newCarePoint.x < width && newCarePoint.y < height - ((height % stepSize)))
+        //{
+        //    //pointInterestPoisitions.Add(newCarePoint);
+        //    pointInterestStructure.Add(new InterestPointStructure(newCarePoint));
+        //    switch (randomPlace)
+        //    {
+        //        case 1:
+        //            generateTile(fountainPrefab, newCarePoint);
+        //            tiles.Add(new Tile(fountainPrefab.name, newCarePoint));
+        //            break;
+        //        case 2:
+        //            generateTile(ChestOfFortune, newCarePoint);
+        //            tiles.Add(new Tile(ChestOfFortune.name, newCarePoint));
+        //            break;
+        //        //case 3:
+        //        //    generateTile(Forge, newCarePoint);
+        //        //    tiles.Add(new Tile(Forge.name, newCarePoint));
+        //        //    break;
+        //        default:
+        //            randomBattlePoint = Random.Range(1, 6);
+        //            switch (randomBattlePoint)
+        //            {
+        //                case 1:
+        //                    generateTile(battlePoint1, newCarePoint);
+        //                    tiles.Add(new Tile(battlePoint1.name, newCarePoint));
+        //                    break;
+        //                case 2:
+        //                    generateTile(battlePoint2, newCarePoint);
+        //                    tiles.Add(new Tile(battlePoint2.name, newCarePoint));
+        //                    break;
+        //                case 3:
+        //                    generateTile(battlePoint3, newCarePoint);
+        //                    tiles.Add(new Tile(battlePoint3.name, newCarePoint));
+        //                    break;
+        //                case 4:
+        //                    generateTile(battlePoint4, newCarePoint);
+        //                    tiles.Add(new Tile(battlePoint4.name, newCarePoint));
+        //                    break;
+        //                case 5:
+        //                    generateTile(battlePoint5, newCarePoint);
+        //                    tiles.Add(new Tile(battlePoint5.name, newCarePoint));
+        //                    break;
+        //            }
+        //            break;
+        //    }
+        //}
     }
     public void InitializateGenerationMap()
     {
-        //using (FileStream fileStream = new FileStream("C:\\text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
-        //{
-        //    fileStream.Seek(0, SeekOrigin.End);
-        //    byte[] buffer = Encoding.Default.GetBytes("zalupa");
-        //    fileStream.Write(buffer, 0, buffer.Length);
-        //}
-        GenerateStartAndBossTiles();
+        //GenerateStartAndBossTiles();
            
-        var endPointTilePosition = endPointTile.GetComponent<RectTransform>().anchoredPosition;
-        var startTilePosition = startTile.GetComponent<RectTransform>().anchoredPosition;
+        //var endPointTilePosition = endPointTile.GetComponent<RectTransform>().anchoredPosition;
+        //var startTilePosition = startTile.GetComponent<RectTransform>().anchoredPosition;
 
-        tiles.Add(new Tile(endPoint.name, endPointTilePosition));
-        tiles.Add(new Tile(start.name, startTilePosition));
+        //tiles.Add(new Tile(endPoint.name, endPointTilePosition));
+        //tiles.Add(new Tile(start.name, startTilePosition));
 
         var carePoint = startTilePosition;
         var countInterestPoint = Random.Range(5, 10);
@@ -294,20 +352,6 @@ public class generateMapScript : Map
         pointInterestStructure.Add(new InterestPointStructure(startTilePosition));
         var index = pointInterestPoisitions.Count() - 1;
         var countCycle = 0;
-        while (!roadToBoss)
-        {
-            countCycle++;
-            var countPoints = pointInterestStructure.Count();
-            for (int i = 0; i < countPoints; i++)
-            {
-                MapGenerator(pointInterestStructure[i]);
-            }
-            if (countCycle > 100)
-            {
-                Debug.Log("Мы закциклились епта");
-                break;
-            }
-        }
         for (int i = 0; i < height; i += (int)stepSize)
         {
             for (int j = 0; j < width; j += (int)stepSize)
@@ -384,7 +428,9 @@ public class generateMapScript : Map
             else
                 PlayerPrefs.SetInt("mapLevel", PlayerPrefs.GetInt("mapLevel")+1);
             //InitializePrefabs();
-            InitializateGenerationMap();
+            //InitializateGenerationMap();
+            //GenerateSquare(width - ((width % stepSize)), height - ((height % stepSize)));
+            MapGenerator();
         }
         else
             GenerateMapFromFile();
