b5558f037e11c968c624f03177c24f3aa5509e3a
Aleksej Lapin <alapin@conteq.com>
2025-02-26 11:18:32 +0300

    король, дракон, фоновая музыка + некие фиксы

---
+++ Assets/Scripts/MusicScripts/DragonSounds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class DragonSounds : MonoBehaviour
+{
+    public GameObject dragonRoarObject;
+    public GameObject dragonWingsObject;
+    public void PlayDragonRoar()
+    {
+        dragonRoarObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void PlayDragonWings()
+    {
+        dragonWingsObject.GetComponent<AudioSource>().Play();
+    }
+}
+
