281c4f97825939e747425931bd5e87d2e3b08473
Aleksej Lapin <alapin@conteq.com>
2025-02-28 21:42:17 +0300

    правки после мержа

---
+++ Assets/Scripts/LapinTest/FPSDisplay.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public TextMeshPro fpsText; // Ссылка на UI текст для отображения FPS
+    private float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f; // Вычисляем среднее время между кадрами
+
+        // Обновляем текстовое поле с текущим FPS
+        if (fpsText != null)
+        {
+            fpsText.text = "FPS: " + Mathf.Ceil(1.0f / deltaTime).ToString();
+        }
+    }
+}
