607c4e7ca296c723449bde2772dc44f4ca8745d8
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-08 14:36:19 +0300

    добавление врагов на экран боя

--- Assets/Scripts/PlayerScripts/CreatePlayerStatic.cs
+++ Assets/Scripts/PlayerScripts/CreatePlayerStatic.cs
@@ -5,58 +5,66 @@ using UnityEngine.UI;
 public class CreatePlayerStatic : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private bool timer_locked_out = true;
+    private float timer = 3f;
+    private float timer_cooldown = 3f;
     void Start()
     {
         InstantinateCharacterModel();
     }
 
+    private void CoolDown()
+    {
+        if (timer_locked_out == true)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = timer_cooldown;
+                timer_locked_out = false;
+            }
+        }
+    }
+
+    private void ActivateAnimationAttack()
+    {
+        if (!timer_locked_out)
+        {
+            timer_locked_out = true;
+            GetComponentInChildren<Animator>().Play("Attack1");
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-
+        CoolDown();
+        ActivateAnimationAttack();
     }
 
-    public bool isDemon()
-    {
-        var player = GameObject.FindGameObjectWithTag("Player");
-        if (player.GetComponent<CharacterStats>().playerTime >= 9)
-            return true;
-        else
-            return false;
-    }
 
     void InstantinateCharacterModel()
     {
         switch (gameObject.name)
         {
             case "Character":
-                //Instantiate(Resources.Load<GameObject>(PlayerPrefs.GetString("characterClass") + "Static"), gameObject.transform.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Main Canvas").transform);
+                //Assets/Resources/BattleChars/Enemys1/Crystall Guard ForBattle.prefab
+                Instantiate(Resources.Load<GameObject>("BattleChars/Player/" + PlayerPrefs.GetString("characterClass") + "ForBattle"), gameObject.transform);
                 break;
-                case "CharacterEnemy":
-                if (Resources.Load<GameObject>(PlayerPrefs.GetString("enemyName")) != null)
+            case "CharacterEnemy":
+                //Debug.Log(PlayerPrefs.GetString("enemyName"));
+                if (Resources.Load<GameObject>("BattleChars/Enemys1/" + PlayerPrefs.GetString("enemyName").Replace("(Clone)","") + " ForBattle") != null)
                 {
-                    var enemy = Instantiate(Resources.Load<GameObject>(PlayerPrefs.GetString("enemyName")), gameObject.transform.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Main Canvas").transform);
-                    enemy.transform.localScale = new Vector3(-5, 5, 5);
-                    var ap = enemy.GetComponent<RectTransform>().anchoredPosition;
-                    ap = new Vector2(ap.x + enemy.GetComponent<RectTransform>().rect.width * Math.Abs(enemy.transform.localScale.x) / 2
-                        , ap.y - enemy.GetComponent<RectTransform>().rect.height * enemy.transform.localScale.y / 2);
-                    enemy.GetComponent<Image>().enabled = false;
-
-
-                    if (isDemon())
-                    {
-                        for (int i = 0; i < enemy.transform.childCount; i++)
-                        {
-                            if (enemy.transform.GetChild(i).name == "pointInterestBattleDemon")
-                            {
-                                enemy.transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = true;
-                            }
-                            if (enemy.transform.GetChild(i).name == "pointInterestBattle")
-                            {
-                                enemy.transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = false;
-                            }
-                        }
-                    }
+                    //Debug.Log(PlayerPrefs.GetString("enemyName"));
+                    //var enemy = 
+                    Instantiate(Resources.Load<GameObject>("BattleChars/Enemys1/" + PlayerPrefs.GetString("enemyName").Replace("(Clone)", "") + " ForBattle"), gameObject.transform);
+                    //var ap = enemy.GetComponent<RectTransform>().anchoredPosition;
+                    //ap = new Vector2(ap.x + enemy.GetComponent<RectTransform>().rect.width * Math.Abs(enemy.transform.localScale.x) / 2
+                    //    , ap.y - enemy.GetComponent<RectTransform>().rect.height * enemy.transform.localScale.y / 2);
+                    //enemy.GetComponent<Image>().enabled = false;
                 }
                 break;
         }
