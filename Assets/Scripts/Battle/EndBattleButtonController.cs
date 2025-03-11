023d8a7fc7d942018dc8263095b8078522c4939d
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-11 14:02:00 +0300

    fixes, battle hide win/lose menu

---
+++ Assets/Scripts/Battle/EndBattleButtonController.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndBattleButtonController : MonoBehaviour
+{
+    public GameObject canvas1, canvas2, canvas3, canvas4;
+    
+    public void ToogleHide()
+   {
+        canvas1.SetActive(!canvas1.activeSelf);
+        if (PlayerPrefs.GetInt("WinLose") == 1)
+        {
+            canvas2.SetActive(!canvas2.activeSelf);
+        }
+        if (PlayerPrefs.GetInt("WinLose") == 2)
+        {
+            canvas3.SetActive(!canvas2.activeSelf);
+        }
+        canvas4.SetActive(!canvas4.activeSelf);
+    }
+
+    
+}
