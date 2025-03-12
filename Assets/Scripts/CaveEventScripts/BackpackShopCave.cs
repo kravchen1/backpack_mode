a9fe0e3510b10c9e60a669752221f7f04e6ff4e0
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-12 11:15:33 +0300

    сцена магаизна кейва донастройка

---
+++ Assets/Scripts/CaveEventScripts/BackpackShopCave.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BackpackShopCave : EventParent
+{
+    private GameObject player;
+    private bool isPlayerInTrigger = false;
+
+    private void OnTriggerEnter2D()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        isPlayerInTrigger = true;
+        if(isShowPressE)
+        {
+            GetComponent<AudioSource>().Play();
+            SetActivePressE(isShowPressE);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPlayerInTrigger = false;
+        SetActivePressE(false);
+    }
+
+    public void ActivateShop()
+    {
+        
+        PlayerPrefs.SetFloat("PostionMapX", player.GetComponent<RectTransform>().anchoredPosition.x);
+        PlayerPrefs.SetFloat("PostionMapY", player.GetComponent<RectTransform>().anchoredPosition.y);
+        PlayerPrefs.SetString("currentLocation", SceneManager.GetActiveScene().name);
+
+
+        //SceneManager.LoadScene("BackPackShop");
+        SceneLoader.Instance.LoadScene("BackPackShopCave1");
+    }
+
+
+
+    
+
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E) && isShowPressE)
+        {
+            ActivateShop();
+        }
+    }
+}
+
