297ef92a3b01aad802f609fe868b89292b873f92
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-12 10:50:22 +0300

    обучение про наведение

---
+++ Assets/Scripts/Interface/MainMenu/ChooseCharacter/EducationGlobalMapController.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EducationGlobalMapController : MonoBehaviour
+{
+    public GameObject Education;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("EducationGlobalMap") == 0)
+        {
+            OpenEducation();
+            PlayerPrefs.SetInt("EducationGlobalMap", 1);
+        }
+    }
+
+    public void CloseEducation()
+    {
+        Education.SetActive(false);
+    }
+
+    public void OpenEducation()
+    {
+        Education.SetActive(!Education.activeSelf);
+    }
+}
