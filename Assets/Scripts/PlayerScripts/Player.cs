4d859f26a1aa55d96bc7eef35635fc4b165a035a
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-19 12:55:42 +0300

    fix gps tracker

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -75,10 +75,7 @@ public class Player : MonoBehaviour
         arrowRectTransform = transform.GetChild(1).GetComponent<RectTransform>();
         PlayerPrefs.SetInt("clickEnemy", 0);
 
-        if (PlayerPrefs.HasKey("QuestTableActive") && PlayerPrefs.GetInt("QuestTableActive") == 0)
-        {
-            arrowRectTransform.gameObject.SetActive(false);
-        }
+        
 
     }
     void LoadCharacterStats()
@@ -290,7 +287,15 @@ public class Player : MonoBehaviour
     {
         if (needGPSTracker && targets.Count > 0)
         {
-            arrowRectTransform.gameObject.SetActive(true);
+            if (PlayerPrefs.HasKey("QuestTableActive") && PlayerPrefs.GetInt("QuestTableActive") == 0)
+            {
+                arrowRectTransform.gameObject.SetActive(false);
+            }
+            else
+            {
+                arrowRectTransform.gameObject.SetActive(true);
+            }
+            
             float minDistance = 99999f;
             int indexMinDestance = 0;
             float Distance = 0;
