ed388fb970fb2569f2cd30943c6a3c5133c06659
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-03-28 18:03:59 +0300

    фиксы, начало новых шрифтов

--- Assets/Scripts/ForgeScripts/ForgeItemScripts/DescriptionScripts/DescriptionItemArmor.cs
+++ Assets/Scripts/ForgeScripts/ForgeItemScripts/DescriptionScripts/DescriptionItemArmor.cs
@@ -15,67 +15,62 @@ public class DescriptionItemArmor : DescriptionItem
 
 
 
-    private void Start()
-    {
-        SetTextBody();
-    }
-
-    public override void SetTextBody()
-    {
-        SetWeight();
-        SetIconDescriptions();
-        SetFont();
-        ItemsText itemText = LocalizationManager.Instance.GetTextItem(settingLanguage, originalName);
-        SetTypeAndRarity(itemText);
+    //public override void SetTextBody()
+    //{
+    //    SetWeight();
+    //    SetIconDescriptions();
+    //    SetFont();
+    //    ItemsText itemText = LocalizationManager.Instance.GetTextItem(settingLanguage, originalName);
+    //    SetTypeAndRarity(itemText);
 
 
 
-        // Загружаем Sprite Asset
-        TMP_SpriteAsset spriteAsset = Resources.Load<TMP_SpriteAsset>("Icons/iconsAtlas");
+    //    // Загружаем Sprite Asset
+    //    TMP_SpriteAsset spriteAsset = Resources.Load<TMP_SpriteAsset>("Icons/iconsAtlas");
 
-        if (spriteAsset == null)
-        {
-            Debug.LogError($"SpriteAsset not found at path: \"Icons/iconsAtlas\"");
-            return;
-        }
+    //    if (spriteAsset == null)
+    //    {
+    //        Debug.LogError($"SpriteAsset not found at path: \"Icons/iconsAtlas\"");
+    //        return;
+    //    }
 
-        textBody.spriteAsset = spriteAsset;
+    //    textBody.spriteAsset = spriteAsset;
 
 
 
-        string patternFigure = @"\{\d+\}";
-        Regex regexFigrue = new Regex(patternFigure);
+    //    string patternFigure = @"\{\d+\}";
+    //    Regex regexFigrue = new Regex(patternFigure);
 
-        // Поиск всех совпадений
-        MatchCollection matchesFigure = regexFigrue.Matches(itemText.description);
+    //    // Поиск всех совпадений
+    //    MatchCollection matchesFigure = regexFigrue.Matches(itemText.description);
 
-        // Количество вхождений
-        int count = matchesFigure.Count;
+    //    // Количество вхождений
+    //    int count = matchesFigure.Count;
 
-        switch (count)
-        {
-            case 0:
-                textBody.text = itemText.description;
-                break;
-            case 1:
-                textBody.text = string.Format(itemText.description, GetField(0));
-                break;
-            case 2:
-                textBody.text = string.Format(itemText.description, GetField(0), GetField(1));
-                break;
-            case 3:
-                textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2));
-                break;
-            case 4:
-                textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2), GetField(3));
-                break;
-            case 5:
-                textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2), GetField(3), GetField(4));
-                break;
-        }
+    //    switch (count)
+    //    {
+    //        case 0:
+    //            textBody.text = itemText.description;
+    //            break;
+    //        case 1:
+    //            textBody.text = string.Format(itemText.description, GetField(0));
+    //            break;
+    //        case 2:
+    //            textBody.text = string.Format(itemText.description, GetField(0), GetField(1));
+    //            break;
+    //        case 3:
+    //            textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2));
+    //            break;
+    //        case 4:
+    //            textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2), GetField(3));
+    //            break;
+    //        case 5:
+    //            textBody.text = string.Format(itemText.description, GetField(0), GetField(1), GetField(2), GetField(3), GetField(4));
+    //            break;
+    //    }
 
-        // Принудительное обновление (на всякий случай)
-        textBody.ForceMeshUpdate();
+    //    // Принудительное обновление (на всякий случай)
+    //    textBody.ForceMeshUpdate();
 
-    }
+    //}
 }
