bff0e34b1f2c44d40e95519fd23a0a304c7caf35
DESKTOP-7VIBHRB\Денис <den740295@mail.ru>
2025-04-22 18:03:57 +0300

    приближение отдаление камеры

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
 
 public class MoveCamera : MonoBehaviour
 {
     private GameObject player;
     public float minX = 155, maxX = 1735, minY = 58, maxY = 980;
+    public float minScroolZ = 20f, maxScrollZ = 100f;
+    public float countScroll = 1f;
     public float needCamSdvigX = 0;
     public float needCamSdvigY = 0;
     //public float durationMove = 1f;
@@ -17,6 +20,7 @@ public class MoveCamera : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         playerRectTransform = player.GetComponent<RectTransform>();
         Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("IgnoreMouse"));
+        Camera.main.orthographicSize = PlayerPrefs.GetFloat("orthographicSize" + SceneManager.GetActiveScene().name, 100f);
     }
 
     
@@ -40,6 +44,29 @@ public class MoveCamera : MonoBehaviour
     private void Update()
     {
         DefaultUpdate();
+
+        // Получаем значение прокрутки колёсика мыши
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollData < 0)
+        {
+            //up
+            if (Camera.main.orthographicSize < maxScrollZ)
+            {
+                Camera.main.orthographicSize += countScroll;
+                PlayerPrefs.SetFloat("orthographicSize" + SceneManager.GetActiveScene().name, Camera.main.orthographicSize);
+            }
+        }
+        else if (scrollData > 0)
+        {
+            //down
+            if(Camera.main.orthographicSize > minScroolZ)
+            {
+                Camera.main.orthographicSize -= countScroll;
+                PlayerPrefs.SetFloat("orthographicSize" + SceneManager.GetActiveScene().name, Camera.main.orthographicSize);
+            }
+            
+        }
     }
 
 
