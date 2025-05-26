4edd06fa93aec5513b53d6fdb97df2096a348bf8
Aleksej Lapin <alapin@conteq.com>
2025-05-26 18:11:20 +0300

    дофикс карты и дискорда

---
+++ Assets/Scripts/Interface/MainMenu/ButtonLink.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ButtonLink : MonoBehaviour
+{
+    public float glowFadeDuration = 0.3f;
+    public void OnMouseEnter()
+    {
+        transform.DOScale(1.05f, glowFadeDuration).SetEase(Ease.OutBack);
+    }
+    public void OnMouseExit()
+    {
+        transform.DOScale(1f, glowFadeDuration).SetEase(Ease.InOutSine);
+    }
+}
